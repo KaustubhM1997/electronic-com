fb5909b54c832518781d789bbaa1b75c004f50dd
Kaustubh Manglurkar <46368989+KaustubhM1997@users.noreply.github.com>
2022-03-19 00:12:59 +0530

    ecom: home-page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import logo from "./logo.png";
+import {Routes, Route, Link} from "react-router-dom";
+import { Home } from "../src/pages/Home/Home";
 
 function App() {
   return (
+    
+    <Routes>
+
+      <Route path= "/" element = {<Home/>}></Route>
 
 
-
-    <h1>Kaustubh is the best</h1>
+    </Routes>
     
   );
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,99 @@
+import "./home.css";
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+const Home = () => {
+  return (
+    <>
+      <nav className="navigation-container">
+        <div className="nav-brand">
+          <Link className="header-style" to="/">
+            NOZAMA
+          </Link>
+          <i className="fa-solid fa-arrow-left-long"></i>
+        </div>
+
+        <div className="input-search">
+          <input className="search" placeholder="Search" type="search" />
+        </div>
+
+        <ul className="nav-pills">
+          <button className="nav-item button">
+            {" "}
+            <Link className="login" to="/pages/authentication-pages/login.html">
+              Login
+            </Link>{" "}
+          </button>
+
+          <div className="badge-element nav-item">
+            <Link to="/pages/wishlist-management/wishlist-management.html">
+              <i className="fa-regular fa-heart icon"></i>
+            </Link>
+
+            <span className="notif-cart">12</span>
+          </div>
+
+          <div className="badge-element nav-item">
+            <Link to="/pages/cart-management/cart-management.html">
+              <i className="fa-solid fa-cart-arrow-down icon"></i>
+            </Link>
+
+            <span className="notif-cart">7</span>
+          </div>
+        </ul>
+      </nav>
+
+      <section className="container-img">
+        <img className="main-image" src="/assets/ecommerce-main.jpg" alt="" />
+
+        <button className="button primary">   <Link
+          className="login"
+          to="/pages/product-listing/product-listing.html"
+        >
+          Shop Now
+        </Link></button>
+
+     
+      </section>
+      <div className="sub-heading">
+        <h2 className="h2">Shop by Category</h2>
+      </div>
+      <div className="card-container">
+        <div className="men-card">
+          <p className="card-heading">Men</p>
+
+          <img className="img-card adjust" src="/assets/men-nav.webp" alt="" />
+        </div>
+
+        <div className="women-card">
+          <p className="card-heading">Women</p>
+
+          <img
+            className="img-card adjust"
+            src="/assets/women-nav.webp"
+            alt=""
+          />
+        </div>
+
+        <div className="kids-card">
+          <p className="card-heading">Kids</p>
+
+          <img className="img-card adjust" src="/assets/kids-nav.webp" alt="" />
+        </div>
+
+        <div className="best-card">
+          <p className="card-heading">Best sellers</p>
+
+          <img
+            className="img-card adjust"
+            src="../assets/best-seller nav.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export { Home };
