d0e6bd0528c98f4fd47b94f5406a8f128100fd7f
Kaustubh Manglurkar <46368989+KaustubhM1997@users.noreply.github.com>
2022-03-24 15:34:20 +0530

    login: UI done

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,17 +1,58 @@
 import "../Home/home.css";
 import React from "react";
-import { Nav } from "../../components/Nav/nav";
 import "./login.css";
-import { Footer } from "../../components/Footer/footer";
+
 
 const Login = () => {
   return (
     <>
-      <Nav />
 
-      <div className="sub-heading title">
-        <h2 className="h2">Coming soon</h2>
-      </div>
+
+{/* <!-- LOGIN CARD --> */}
+
+<div className="login-container">
+
+    <h2 className="login-header">Login</h2>
+
+    <div className="login-details">
+
+        <div className="login-subdetails">
+
+            <p className="subdetails-header">Email address</p>
+            <input className="login-input" placeholder="kaustubh@gmail.com" type="text"/>
+        </div>
+
+        <div className="login-subdetails">
+
+            <p  className="subdetails-header">Password</p>
+            <input className="login-input" placeholder="********" type="text"/>
+        </div>
+
+        <div className="forgot-password">
+
+            <input type="checkbox"/> <p className="password-remember">Remember me</p>
+
+            <p className="forgot-password-text">Forgot your Password?</p>
+        </div>
+
+        <div className="btn-container">
+
+            <button className="login-btn">Login</button>
+        </div>
+
+        <div className="new-account">
+
+            <p> <a className="text" href="/pages/authentication-pages/signup.html">Create New Account</a> </p>
+        </div>
+
+
+
+    </div>
+
+
+</div>
+
+    
     </>
   );
 };
