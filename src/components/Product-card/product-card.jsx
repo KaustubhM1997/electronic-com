f897dffe0b51423337bc6ca3012c07b3639fa7ae
Kaustubh Manglurkar <46368989+KaustubhM1997@users.noreply.github.com>
2022-03-30 00:04:13 +0530

    refactor: prettier

--- src/components/Product-card/product-card.jsx
+++ src/components/Product-card/product-card.jsx
@@ -6,43 +6,34 @@ import axios from "axios";
 import { useState } from "react";
 import { useWishlist } from "../../contexts/wishlist-context";
 
-const ProductCard = ({productcard}) => { //destructuring the product card object from props
-  const {state, dispatch} = useWishlist();
+const ProductCard = ({ productcard }) => {
+  //destructuring the product card object from props
+  const { state, dispatch } = useWishlist();
 
-   //extracting the wishlist from state where we're storing it
+  //extracting the wishlist from state where we're storing it
 
-   const {wishlist} = state;
-
-
+  const { wishlist } = state;
 
   const { title, price, rating, productImg } = productcard; // destrructuring all the keys from productcard
 
-
-  const {auth: {Authenticated, token}} = useAuth();
+  const {
+    auth: { Authenticated, token },
+  } = useAuth();
 
   const navigate = useNavigate();
   const [errors, setErrors] = useState("");
 
-
- 
-
-
   //this gets triggered when the heart icon is clicked below and the item is not present in the wishlist array
-
 
   // console.log(productcard);
 
-
   const addToWishlistHandler = async (productcard) => {
-
-    
-
     //as soon as the user logs in, the pre-exisiting wishlist items are shown and then anything the user adds from product paage is added below when dispatch fires
     if (Authenticated) {
       try {
         const response = await axios.post(
           "/api/user/wishlist",
-          {product: productcard}, //product is the predefined key that we pass the value object to
+          { product: productcard }, //product is the predefined key that we pass the value object to
           {
             headers: { authorization: token },
           }
@@ -57,17 +48,15 @@ const ProductCard = ({productcard}) => { //destructuring the product card object
           payload: response.data.wishlist, //gets added to the wishlist arraye
         });
       } catch (errors) {
-        // setErrors(errors.response.data); 
-        console.log(errors)
+        // setErrors(errors.response.data);
+        console.log(errors);
       }
     } else {
       navigate("/login-page");
     }
   };
 
-
   //this gets triggered when the item id clicked is equal to the product id that's present in the wishlist. This removes the product from wishlist
-
 
   const removeFromWishlistHandler = async (id) => {
     if (Authenticated) {
@@ -90,50 +79,56 @@ const ProductCard = ({productcard}) => { //destructuring the product card object
   };
 
   return (
-  
-      <div className="card-image">
-        <div className="card-vertical">
-          <div className="card-vertical-info flex-column">
-            <img
-              className="img-responsive adjust-image"
-              src={productImg}
-              alt="shopping-item"
-            />
-            <p className="card-text">{title}</p>
+    <div className="card-image">
+      <div className="card-vertical">
+        <div className="card-vertical-info flex-column">
+          <img
+            className="img-responsive adjust-image"
+            src={productImg}
+            alt="shopping-item"
+          />
+          <p className="card-text">{title}</p>
 
-            <div className="rating-badge">
-              {rating}
-              <span className="star-icon">
-                <i className="fa-solid fa-star"></i>
-              </span>
-            </div>
+          <div className="rating-badge">
+            {rating}
+            <span className="star-icon">
+              <i className="fa-solid fa-star"></i>
+            </span>
+          </div>
 
-            {/* <p className="card-subtext">
+          {/* <p className="card-subtext">
               A bit more detail about the product
             </p> */}
-            <p className="card-text card-price">
-              {price}
-              <span className="text-line-through">7,999</span>
-              <span className="discount-percent">40%</span>
-            </p>
-            <button className="btn btn-secondary">Add to Cart</button>
+          <p className="card-text card-price">
+            {price}
+            <span className="text-line-through">7,999</span>
+            <span className="discount-percent">40%</span>
+          </p>
+          <button className="btn btn-secondary">Add to Cart</button>
 
-            {/* The first span checks whether the product is already present in the wishlist array, if yes then on click it removes from wishlisht. Meaning the heart color would already be filled here */}
+          {/* The first span checks whether the product is already present in the wishlist array, if yes then on click it removes from wishlisht. Meaning the heart color would already be filled here */}
 
-           {Authenticated &&  wishlist.find((item) => item._id === productcard._id) ?(<span onClick = {() => removeFromWishlistHandler(productcard._id)} className="card-heart-icon"> 
-            
-           <i class="fa-solid fa-heart"></i> 
-          </span>): (<span onClick = {() => addToWishlistHandler(productcard)} className="card-heart-icon"> 
-            
-          <i className="fa-regular fa-heart"></i>
-          </span>) } 
+          {Authenticated &&
+          wishlist.find((item) => item._id === productcard._id) ? (
+            <span
+              onClick={() => removeFromWishlistHandler(productcard._id)}
+              className="card-heart-icon"
+            >
+              <i class="fa-solid fa-heart"></i>
+            </span>
+          ) : (
+            <span
+              onClick={() => addToWishlistHandler(productcard)}
+              className="card-heart-icon"
+            >
+              <i className="fa-regular fa-heart"></i>
+            </span>
+          )}
 
-            {/* // the 2nd span triggers the handler when heart icon is clicked and pass our productcard(products) that we've got from product listing */}
-
-            
-          </div>
+          {/* // the 2nd span triggers the handler when heart icon is clicked and pass our productcard(products) that we've got from product listing */}
         </div>
       </div>
+    </div>
   );
 };
 
